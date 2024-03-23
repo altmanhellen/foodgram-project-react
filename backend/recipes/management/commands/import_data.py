5b55599ee17a831b7cc344e25244996b89365834
Elena Altman <altmanhellen@gmail.com>
2024-03-23 20:25:45 +0200

    3

--- backend/recipes/management/commands/import_data.py
+++ backend/recipes/management/commands/import_data.py
@@ -13,22 +13,23 @@ class Command(BaseCommand):
         parser.add_argument('path', type=str, help='Пусть к JSON-файлу')
 
     def handle(self, *args, **options):
-        if Tag.objects.exists() or Ingredient.objects.exists():
-            print('Данные уже загружены')
-            return
         path = options['path']
-        with open(path, 'r', encoding='utf-8') as file:
-            data = json.load(file)
-            if 'color' in data[0]:
-                for line in data:
-                    Tag.objects.create(
-                        name=line['name'],
-                        color=line['color'],
-                        slug=line['slug']
-                    )
-            if 'measurement_unit' in data[0]:
-                for line in data:
-                    Ingredient.objects.create(
-                        name=line['name'],
-                        measurement_unit=line['measurement_unit']
-                    )
+        if not Tag.objects.exists():
+            with open(path, 'r', encoding='utf-8') as file:
+                data = json.load(file)
+                if 'color' in data[0]:
+                    for line in data:
+                        Tag.objects.create(
+                            name=line['name'],
+                            color=line['color'],
+                            slug=line['slug']
+                        )
+        if not Ingredient.objects.exists():
+            with open(path, 'r', encoding='utf-8') as file:
+                data = json.load(file)
+                if 'measurement_unit' in data[0]:
+                    for line in data:
+                        Ingredient.objects.create(
+                            name=line['name'],
+                            measurement_unit=line['measurement_unit']
+                        )
